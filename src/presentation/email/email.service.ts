8507fa0172fa9fa9a07d2cfc53dfe93f85bd0d57
Bryan Guillén <bryanguillenlds@gmail.com>
2025-10-07 16:48:55 -0400

    Implemented Email Service.
    - Added MAILER_SERVICE configuration to .env.template for email service setup.
    - Integrated nodemailer package for email functionality.
    - Updated LogEntity to include origin property for better log context.
    - Refactored log creation in CheckService to utilize the new origin option.

---
+++ src/presentation/email/email.service.ts
@@ -0,0 +1,55 @@
+import nodemailer from "nodemailer";
+import { envs } from "../../config/plugins/env.plugin";
+
+interface Attachment {
+  filename: string;
+  path: string;
+}
+
+interface SendEmailOptions {
+  to: string | string[];
+  subject: string;
+  htmlBody: string;
+  attachments?: Attachment[];
+}
+
+export class EmailService {
+  private transporter = nodemailer.createTransport({
+    service: envs.MAILER_SERVICE,
+    auth: {
+      user: envs.MAILER_EMAIL,
+      pass: envs.MAILER_SECRET_KEY,
+    },
+  });
+
+  async sendEmail(options: SendEmailOptions): Promise<boolean> {
+    const { to, subject, htmlBody, attachments = [] } = options;
+
+    try {
+      const sent = await this.transporter.sendMail({
+        to,
+        subject,
+        html: htmlBody,
+        attachments,
+      });
+
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+
+  async sendEmailWithFSLogs(to: string | string[]) {
+    const subject = "Server Logs";
+    const htmlBody = "<h1>Server Logs</h1>";
+    const attachments: Attachment[] = [
+      {
+        filename: "logs-medium.log",
+        path: "./logs/logs-medium.log",
+      },
+    ];
+
+    return this.sendEmail({ to, subject, htmlBody, attachments });
+  }
+}
